99ee56f685136290235ac21d8840c27e2ae9bfff
NeNorvalls <honeygeeh27@gmail.com>
2023-11-06 05:00:05 +0100

    Update homePage.js

--- homePage.js
+++ homePage.js
@@ -104,3 +104,11 @@ document.addEventListener('DOMContentLoaded', () => {
     window.location.href = 'cart.html';
   };
 });
+
+const portfolioURL = 'https://nenorvalls-projects-showcase.netlify.app/';
+
+const goBackButton = document.getElementById('goBackButton');
+
+goBackButton.addEventListener('click', function() {
+  window.location.href = portfolioURL;
+});
